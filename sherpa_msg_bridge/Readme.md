8d48b3ab0cc1b0efaa74bedfff23613b2bb3324e
Sebastian Blumenthal <blumenthal@locomotec.com>
2016-02-08 11:39:54 +0100

    Added skeleton for SHERPA message conversion block.

---
+++ sherpa_msg_bridge/Readme.md
@@ -0,0 +1,82 @@
+Tutorial: How to use the CZMQ-UBX bridge
+========================
+
+About
+---
+This bridge enables you to link a microblx application to other (possibly remote) applications using CZMQ sockets. CZMQ is a high-level C binding for ØMQ, a networking library with _sockets on steroids_.
+
+In this tutorial PUB-SUB sockets are used to demonstrate the communcation between two microblx applications (a publisher and a subscriber).
+Configuration parameters allow you to change the protocol (e.g. tcp://, ipc://, pgm://, inproc://) and the address and port, if relevant.
+
+More information on CZMQ and ØMQ itself can be found [here](http://zguide.zeromq.org/page:all) and [here](http://czmq.zeromq.org/manual:_start). 
+Prerequisites
+-------------
+### Install ubx dependencies: luajit, libluajit-5.1-dev, clang++, clang
+```sh
+~/workspace$ sudo apt-get install luajit, libluajit-5.1-dev, clang
+```
+### Install ubx and follow instructions in the [quickstart](http://ubxteam.github.io/quickstart/).
+
+```sh
+~/workspace$ git clone https://github.com/UbxTeam/microblx.git
+~/workspace$ cd microblx
+```
+
+### Install ubx cmake:
+
+```sh
+~/workspace$ git clone https://github.com/haianos/microblx_cmake.git
+```
+### Set some environment variables and run the env.sh script (e.g. in your .bashrc):
+
+```sh
+export UBX_ROOT=~/workspace/microblx
+export UBX_MODULES=~/workspace/install/lib/microblx
+source $UBX_ROOT/env.sh
+```
+
+### Install CZMQ:
+
+```sh
+~/workspace$ git clone git://github.com/zeromq/czmq
+~/workspace$ cd czmq
+~/workspace/czmq$ ./autogen.sh
+~/workspace/czmq$ ./configure
+~/workspace/czmq$ make
+~/workspace/czmq$ sudo make install
+```
+
+Building & running the CZMQ bridge example
+---
+
+After building the code, you can run an example configuration in the root directory.
+This will connect at the publisher side a random number generator block with the sender block and at the subscriber side the receiver block with
+a hexdump block. Sender and receiver are connected through the configurable CZMQ PUB-SUB sockets.
+
+### Clone the ubx repository:
+```sh
+~/workspace$ git clone https://github.com/maccradar/ubx.git
+~/workspace$ cd ubx/czmq_bridge
+~/workspace/ubx/czmq_bridge$ mkdir build
+~/workspace/ubx/czmq_bridge$ cd build
+~/workspace/ubx/czmq_bridge/build$ cmake ..
+~/workspace/ubx/czmq_bridge/build$ make
+~/workspace$ cd ..
+~/workspace$ ./run_czmq_bridge.sh
+```
+Use the web interface (http://localhost:8888/) to inspect and start the example.
+
+### PUB-SUB sockets in CZMQ
+![PUB-SUB](https://github.com/imatix/zguide/raw/master/images/fig4.png "PUB-SUB")
+
+This image is (c) 2014 iMatix Corporation, licensed under [cc-by-sa 3.0](http://creativecommons.org/licenses/by-sa/3.0/).
+
+License
+---
+
+The microblx software in this tutorial is licensed under BSD, CZMQ is open source under MPL v2 license, ØMQ is free software licensed under the LGPL.
+
+Authors
+-----
+
+Sebastian Blumenthal & Johan Philips
---
+++ sherpa_msg_bridge/src/sherpa_msg_bridge.cpp
@@ -0,0 +1,198 @@
+#include "sherpa_msg_bridge.hpp"
+
+/* ZMQ includes */
+#include <czmq.h>
+#include <iostream>
+
+UBX_MODULE_LICENSE_SPDX(BSD-3-Clause)
+
+const unsigned int DEFAULT_BUFFER_LENGTH = 20000;
+
+static void receiver_actor(zsock_t *pipe, void *args);
+/* define a structure for holding the block local state. By assigning an
+ * instance of this struct to the block private_data pointer (see init), this
+ * information becomes accessible within the hook functions.
+ */
+struct zmq_server_info
+{
+	/* add custom block local data here */
+	// ZMQ server
+	zctx_t *ctx;
+
+	void *server; //
+
+	zsock_t* server_socket;
+
+	// actor running in separate thread to receive the messages
+	zactor_t* actor;
+
+
+    // Data buffer fpr input port
+    unsigned char* buffer;
+
+    // Length of the buffer
+    unsigned long buffer_length;
+
+	/* this is to have fast access to ports for reading and writing, without
+	 * needing a hash table lookup */
+	struct zmq_server_port_cache ports;
+};
+
+/* init */
+int zmq_server_init(ubx_block_t *b)
+{
+	int ret = -1;
+	struct zmq_server_info *inf;
+	unsigned int tmplen;
+	char *connection_spec_str;
+
+	// CZMQ socket for subscriber
+	zsock_t* server_socket;
+	/* allocate memory for the block local state */
+	if ((inf = (struct zmq_server_info*)calloc(1, sizeof(struct zmq_server_info)))==NULL) {
+		ERR("zmq_server: failed to alloc memory");
+		ret=EOUTOFMEM;
+		return ret;
+	}
+	b->private_data=inf;
+	update_port_cache(b, &inf->ports);
+
+    inf->buffer_length = DEFAULT_BUFFER_LENGTH; //TODO read from config
+    inf->buffer = new unsigned char [inf->buffer_length];
+
+	connection_spec_str = (char*) ubx_config_get_data_ptr(b, "connection_spec", &tmplen);
+	printf("ZMQ connection configuration for block %s is %s\n", b->name, connection_spec_str);
+
+    //  Create context
+    inf->ctx = zctx_new ();
+
+    //  Create and bind server socket
+//    void* server = zsocket_new (inf->ctx, ZMQ_REP);
+//    zsocket_bind (server, "tcp://%s:%d", "127.0.0.0", 22422);
+
+	// create subscriber socket and subscribe to all messages
+	server_socket = zsock_new_rep(connection_spec_str);
+//	zsock_set_subscribe(sub, "");
+
+	if (!server_socket) {
+		return ret;
+	}
+	// add pointer to subscriber to private data
+	inf->server_socket = server_socket;
+
+	return 0;
+}
+
+/* start */
+int zmq_server_start(ubx_block_t *b)
+{
+	struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data;
+
+	// incoming data is handled by the actor thread
+	zactor_t* actor = zactor_new (receiver_actor, b);
+	inf->actor = actor;
+
+	int ret = 0;
+	return ret;
+}
+
+/* stop */
+void zmq_server_stop(ubx_block_t *b)
+{
+        /* struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data; */
+}
+
+/* cleanup */
+void zmq_server_cleanup(ubx_block_t *b)
+{
+	struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data;
+	// clean up context socket
+	zctx_destroy (&inf->ctx);
+	// clean up actor thread
+	zactor_destroy(&inf->actor);
+	free(b->private_data);
+}
+
+/* step */
+void zmq_server_step(ubx_block_t *b)
+{
+
+    struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data;
+//    std::cout << "zmq_server: Processing a port update" << std::endl;
+
+	/* Read data from port */
+	ubx_port_t* port = inf->ports.zmq_rep;
+	assert(port != 0);
+
+	ubx_data_t msg;
+	checktype(port->block->ni, port->in_type, "unsigned char", port->name, 1);
+	msg.type = port->in_type;
+	msg.len = inf->buffer_length;
+	msg.data = inf->buffer;
+
+//	std::cout << "zmq_server: Reading from port" << std::endl;
+	int read_bytes = __port_read(port, &msg);
+	if (read_bytes <= 0) {
+//		std::cout << "zmq_server: No data recieved from port" << std::endl;
+		return;
+	}
+
+	std::cout << "zmq_server: read bytes = " << read_bytes << std::endl;
+
+	/* Setup ZMQ frame. At this point only single frames are sent. This can be replaced by zmsg_t messages
+        if multi-part messages become necessary*/
+	zframe_t* message = zframe_new(msg.data, read_bytes);
+	std::cout << "Created frame of length " << zframe_size(message) << std::endl;
+
+	/* Send the message */
+	int result = zframe_send(&message, inf->server_socket,0);
+	std::cout << "send message with result " << result << std::endl;
+
+}
+
+int handle_event(zloop_t *loop, zsock_t *reader, void *args) {
+	// initialization
+	ubx_block_t *b = (ubx_block_t *) args;
+	struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data;
+	printf("zmq_server: data available.\n");
+
+	zframe_t *frame = zframe_recv (reader);
+	// print out frame data
+	zframe_print (frame, NULL);
+
+	// move to step function?
+	ubx_type_t* type =  ubx_type_get(b->ni, "unsigned char");
+	ubx_data_t msg;
+	msg.data = (void *)zframe_data(frame);
+	msg.len = zframe_size(frame);
+	msg.type = type;
+
+	//hexdump((unsigned char *)msg.data, msg.len, 16);
+	__port_write(inf->ports.zmq_req, &msg);
+
+	/* Inform potential observers ? */
+
+	// clean up temporary frame object
+	zframe_destroy (&frame);
+
+	return 1;
+}
+
+static void
+receiver_actor (zsock_t *pipe, void *args)
+{
+    // initialization
+    ubx_block_t *b = (ubx_block_t *) args;
+    struct zmq_server_info *inf = (struct zmq_server_info*) b->private_data;
+    printf("zmq_server: actor started.\n");
+    // send signal on pipe socket to acknowledge initialisation
+    zsock_signal (pipe, 0);
+
+    zloop_t *loop = zloop_new ();
+    assert (loop);
+    int rc = zloop_reader (loop, inf->server_socket, handle_event, args);
+    assert (rc == 0);
+    zloop_start (loop);
+
+    zloop_destroy (&loop);
+}
