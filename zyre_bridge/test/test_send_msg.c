e9d331cd9390e64532a26b25cf257f1dc7f4ff6a
Nico_Huebel <nico.huebel@kuleuven.be>
2016-08-11 10:10:12 +0200

    added constructors and destructors for component and query list etc

--- zyre_bridge/test/test_send_msg.c
+++ zyre_bridge/test/test_send_msg.c
@@ -17,6 +17,20 @@ typedef struct _query_t {
         zactor_t *loop;
 } query_t;
 
+typedef struct _component_t {
+	const char* name;
+	const char *localgroup;
+	zyre_t *local;
+	json_t *config;
+	zpoller_t *poller;
+	zlist_t *query_list;
+	int timeout;
+	int no_of_updates;
+	int no_of_queries;
+	int no_of_fcn_block_calls;
+	int alive;
+} component_t;
+
 void query_destroy (query_t **self_p) {
         assert (self_p);
         if(*self_p) {
@@ -24,6 +38,25 @@ void query_destroy (query_t **self_p) {
             free (self);
             *self_p = NULL;
         }
+}
+
+void destroy_component (component_t **self_p) {
+    assert (self_p);
+    if(*self_p) {
+    	component_t *self = *self_p;
+        zyre_destroy (&self->local);
+        json_decref(self->config);
+        zpoller_destroy (&self->poller);
+        //free memory of all items from the query list
+        query_t *it;
+        while(zlist_size (self->query_list) > 0){
+        	it = zlist_pop (self->query_list);
+        	query_destroy(&it);
+        }
+        zlist_destroy (&self->query_list);
+        free (self);
+        *self_p = NULL;
+    }
 }
 
 query_t * query_new (const char *uid, const char *requester, json_msg_t *msg, zactor_t *loop) {
@@ -38,6 +71,93 @@ query_t * query_new (const char *uid, const char *requester, json_msg_t *msg, za
         return self;
 }
 
+component_t* new_component(json_t *config) {
+	component_t *self = (component_t *) zmalloc (sizeof (component_t));
+    if (!self)
+        return NULL;
+    if (!config)
+            return NULL;
+
+    self->config = config;
+
+	self->name = json_string_value(json_object_get(config, "short-name"));
+    if (!self->name) {
+        destroy_component (&self);
+        return NULL;
+    }
+	self->timeout = json_integer_value(json_object_get(config, "timeout"));
+    if (self->timeout <= 0) {
+    	destroy_component (&self);
+        return NULL;
+    }
+
+	self->no_of_updates = json_integer_value(json_object_get(config, "no_of_updates"));
+    if (self->no_of_updates <= 0) {
+    	destroy_component (&self);
+        return NULL;
+    }
+
+	self->no_of_queries = json_integer_value(json_object_get(config, "no_of_queries"));
+    if (self->no_of_queries <= 0) {
+    	destroy_component (&self);
+        return NULL;
+    }
+
+	self->no_of_fcn_block_calls = json_integer_value(json_object_get(config, "no_of_fcn_block_calls"));
+    if (self->no_of_fcn_block_calls <= 0) {
+    	destroy_component (&self);
+        return NULL;
+    }
+	//  Create local gossip node
+	self->local = zyre_new (self->name);
+    if (!self->local) {
+    	destroy_component (&self);
+        return NULL;
+    }
+	printf("[%s] my local UUID: %s\n", self->name, zyre_uuid(self->local));
+
+	/* config is a JSON object */
+	// set values for config file as zyre header.
+	const char *key;
+	json_t *value;
+	json_object_foreach(config, key, value) {
+		const char *header_value;
+		if(json_is_string(value)) {
+			header_value = json_string_value(value);
+		} else {
+			header_value = json_dumps(value, JSON_ENCODE_ANY);
+		}
+		printf("header key value pair\n");
+		printf("%s %s\n",key,header_value);
+		zyre_set_header(self->local, key, "%s", header_value);
+	}
+
+	int rc;
+	if(!json_is_null(json_object_get(config, "gossip_endpoint"))) {
+		//  Set up gossip network for this node
+		zyre_gossip_connect (self->local, "%s", json_string_value(json_object_get(config, "gossip_endpoint")));
+		printf("[%s] using gossip with gossip hub '%s' \n", self->name,json_string_value(json_object_get(config, "gossip_endpoint")));
+	} else {
+		printf("[%s] WARNING: no local gossip communication is set! \n", self->name);
+	}
+	rc = zyre_start (self->local);
+	assert (rc == 0);
+	self->localgroup = strdup("local");
+	zyre_join (self->local, self->localgroup);
+	//  Give time for them to connect
+	zclock_sleep (1000);
+
+	//create a list to store queries...
+	self->query_list = zlist_new();
+	if ((!self->query_list)&&(zlist_size (self->query_list) == 0)) {
+		destroy_component (&self);
+		return NULL;
+	}
+
+	self->alive = 1; //will be used to quit program after answer to query is received
+	self->poller =  zpoller_new (zyre_socket(self->local), NULL);
+	return self;
+}
 
 json_t * load_config_file(char* file) {
     json_error_t error;
@@ -98,50 +218,50 @@ int decode_json(char* message, json_msg_t *result) {
     return 0;
 }
 
-char* send_msg() {
-	/**
-	 * creates a world model update msg
-	 *
-	 * @return the string encoded JSON msg that can be sent directly via zyre. Must be freed by user! Returns NULL if wrong json types are passed in.
-	 */
+//char* send_msg() {
+//	/**
+//	 * creates a world model update msg
+//	 *
+//	 * @return the string encoded JSON msg that can be sent directly via zyre. Must be freed by user! Returns NULL if wrong json types are passed in.
+//	 */
+//
+//    json_t *pl;
+//    pl = json_object();
+//    json_object_set(pl, "@worldmodeltype", json_string("RSGUpdate"));
+//    json_object_set(pl, "operation", json_string("CREATE"));
+//	json_object_set(pl, "parentId", json_string("e379121f-06c6-4e21-ae9d-ae78ec1986a1"));
+//    json_t *node;
+//    node = json_object();
+//    json_object_set(node, "@graphtype", json_string("Node"));
+//	zuuid_t *uuid = zuuid_new ();
+//	assert(uuid);
+//	//printf("UUID: %s\n",zuuid_str_canonical(uuid));
+//    json_object_set(node, "id", json_string(zuuid_str_canonical(uuid)));
+//	json_t *attributes;
+//    attributes = json_array();
+//	json_t *kv;
+//	kv = json_object();
+//	json_object_set(kv, "key", json_string("name"));
+//	json_object_set(kv, "value", json_string("myNode"));
+//	json_array_append_new(attributes, kv);
+//	kv = json_object();
+//	json_object_set(kv, "key", json_string("comment"));
+//	json_object_set(kv, "value", json_string("Some human readable comment on the node."));
+//	json_array_append_new(attributes, kv);
+//	json_object_set(node, "attributes", attributes);
+//    json_object_set(pl, "node", node);
+//    char* ret = json_dumps(pl, JSON_ENCODE_ANY);
+//
+//	json_decref(kv);
+//	json_decref(attributes);
+//	json_decref(node);
+//    json_decref(pl);
+//    return ret;
+//}
 
-    json_t *pl;
-    pl = json_object();
-    json_object_set(pl, "@worldmodeltype", json_string("RSGUpdate"));
-    json_object_set(pl, "operation", json_string("CREATE"));
-	json_object_set(pl, "parentId", json_string("e379121f-06c6-4e21-ae9d-ae78ec1986a1"));
-    json_t *node;
-    node = json_object();
-    json_object_set(node, "@graphtype", json_string("Node"));
-	zuuid_t *uuid = zuuid_new ();
-	assert(uuid);
-	//printf("UUID: %s\n",zuuid_str_canonical(uuid));
-    json_object_set(node, "id", json_string(zuuid_str_canonical(uuid)));
-	json_t *attributes;
-    attributes = json_array();
-	json_t *kv;
-	kv = json_object();
-	json_object_set(kv, "key", json_string("name"));
-	json_object_set(kv, "value", json_string("myNode"));
-	json_array_append_new(attributes, kv);
-	kv = json_object();
-	json_object_set(kv, "key", json_string("comment"));
-	json_object_set(kv, "value", json_string("Some human readable comment on the node."));
-	json_array_append_new(attributes, kv);
-	json_object_set(node, "attributes", attributes);
-    json_object_set(pl, "node", node);
-    char* ret = json_dumps(pl, JSON_ENCODE_ANY);
-	
-	json_decref(kv);
-	json_decref(attributes);
-	json_decref(node);
-    json_decref(pl);
-    return ret;
-}
-
-char* send_query(char* query_type, json_t* query_params) {
+char* send_query(component_t* self, char* query_type, json_t* query_params) {
 	/**
-	 * creates a query msg for the world model
+	 * creates a query msg for the world model and adds it to the query list
 	 *
 	 * @param string query_type as string containing one of the availble query types ["GET_NODES", "GET_NODE_ATTRIBUTES", "GET_NODE_PARENTS", "GET_GROUP_CHILDREN", "GET_ROOT_NODE", "GET_REMOTE_ROOT_NODES", "GET_TRANSFORM", "GET_GEOMETRY", "GET_CONNECTION_SOURCE_IDS", "GET_CONNECTION_TARGET_IDS"]
 	 * @param json_t query_params json object containing all information required by the query; check the rsg-query-schema.json for details
@@ -156,7 +276,7 @@ char* send_query(char* query_type, json_t* query_params) {
     json_object_set(pl, "query", json_string(query_type));
 	zuuid_t *uuid = zuuid_new ();
 	assert(uuid);
-    //json_object_set(pl, "queryId", json_string(zuuid_str_canonical(uuid)));
+    json_object_set(pl, "queryId", json_string(zuuid_str_canonical(uuid)));
 	
 	if (json_object_size(query_params)>0) {
 		const char *key;
@@ -173,9 +293,14 @@ char* send_query(char* query_type, json_t* query_params) {
 	json_object_set(env, "type", json_string("RSGQuery"));
 	json_object_set(env, "payload", pl);
 	
-
-	//query_t * q = query_new(uuid, peerid, result, file_server);
-	//zlist_append(self->remote_query_list, q);
+	// add it to the query list
+	json_msg_t *msg = (json_msg_t *) zmalloc (sizeof (json_msg_t));
+	msg->metamodel = strdup("SHERPA");
+	msg->model = strdup("RSGQuery");
+	msg->type = strdup("RSGQuery");
+	msg->payload = strdup(json_dumps(pl, JSON_ENCODE_ANY));
+	query_t * q = query_new(zuuid_str_canonical(uuid), zyre_uuid(self->local), msg, NULL);
+	zlist_append(self->query_list, q);
 
     char* ret = json_dumps(env, JSON_ENCODE_ANY);
 	
@@ -183,6 +308,7 @@ char* send_query(char* query_type, json_t* query_params) {
     json_decref(pl);
     return ret;
 }
+
 
 int main(int argc, char *argv[]) {
 
@@ -197,65 +323,11 @@ int main(int argc, char *argv[]) {
     if (config == NULL) {
       return -1;
     }
-    const char *self = json_string_value(json_object_get(config, "short-name"));
-    assert(self);
-    bool verbose = json_is_true(json_object_get(config, "verbose"));
-    int timeout = json_integer_value(json_object_get(config, "timeout"));
-    assert(timeout > 0);
-	
-	int no_of_updates = json_integer_value(json_object_get(config, "no_of_updates"));
-	int no_of_queries = json_integer_value(json_object_get(config, "no_of_queries"));
-	int no_of_fcn_block_calls = json_integer_value(json_object_get(config, "no_of_fcn_block_calls"));
-
-    //  Create local gossip node
-    zyre_t *local = zyre_new (self);
-    assert (local);
-    printf("[%s] my local UUID: %s\n", self, zyre_uuid(local));
-    /* config is a JSON object */
-    // set values for config file as zyre header.
-    const char *key;
-    json_t *value;
-    json_object_foreach(config, key, value) {
-        /* block of code that uses key and value */
-    	const char *header_value;
-    	if(json_is_string(value)) {
-    		header_value = json_string_value(value);
-    	} else {
-    		header_value = json_dumps(value, JSON_ENCODE_ANY);
-    	}
-    	printf("header key value pair\n");
-    	printf("%s %s\n",key,header_value);
-    	zyre_set_header(local, key, "%s", header_value);
+    component_t *self = new_component(config);
+    if (self == NULL) {
+    	return -1;
     }
-
-    int rc;
-    if(!json_is_null(json_object_get(config, "gossip_endpoint"))) {
-    	//rc = zyre_set_endpoint (local, "%s", json_string_value(json_object_get(config, "local_endpoint")));
-    	//assert (rc == 0);
-    	//printf("[%s] using gossip with local endpoint %s\n", self, json_string_value(json_object_get(config, "local_endpoint")));
-    	//  Set up gossip network for this node
-    	zyre_gossip_connect (local, "%s", json_string_value(json_object_get(config, "gossip_endpoint")));
-    	printf("[%s] using gossip with gossip hub '%s' \n", self,json_string_value(json_object_get(config, "gossip_endpoint")));
-    } else {
-    	printf("[%s] WARNING: no local gossip communication is set! \n", self);
-    }
-    rc = zyre_start (local);
-    assert (rc == 0);
-    char* localgroup = strdup("local");
-    zyre_join (local, localgroup);
-    //  Give time for them to connect
-    zclock_sleep (1000);
-
-    if (verbose)
-        zyre_dump (local);
-
-    //create a list to store queries...
-    zlist_t *query_list = zlist_new();
-    assert (query_list);
-    assert (zlist_size (query_list) == 0);
-
-    int alive = 1; //will be used to quit program after answer to query is received
-    zpoller_t *poller =  zpoller_new (zyre_socket(local), NULL);
+    printf("[%s] component initialised!\n", self->name);
 
     //check if there is at least one component connected
     zlist_t * tmp = zlist_new ();
@@ -264,32 +336,30 @@ int main(int argc, char *argv[]) {
     // timestamp for timeout
     struct timespec ts = {0,0};
     if (clock_gettime(CLOCK_MONOTONIC,&ts)) {
-		printf("[%s] Could not assign time stamp!\n",self);
+		printf("[%s] Could not assign time stamp!\n",self->name);
 	}
     struct timespec curr_time = {0,0};
     while (true) {
-    	printf("[%s] Checking for connected peers.\n",self);
-    	zlist_t * tmp = zyre_peers(local);
-    	printf("[%s] %zu peers connected.\n", self,zlist_size(tmp));
+    	printf("[%s] Checking for connected peers.\n",self->name);
+    	tmp = zyre_peers(self->local);
+    	printf("[%s] %zu peers connected.\n", self->name,zlist_size(tmp));
     	if (zlist_size (tmp) > 0)
     		break;
 		if (!clock_gettime(CLOCK_MONOTONIC,&curr_time)) {
 			// if timeout, stop component
 			double curr_time_msec = curr_time.tv_sec*1.0e3 +curr_time.tv_nsec*1.0e-6;
 			double ts_msec = ts.tv_sec*1.0e3 +ts.tv_nsec*1.0e-6;
-			if (curr_time_msec - ts_msec > timeout) {
-				printf("[%s] Timeout! Could not connect to other peers.\n",self);
-				zyre_stop (local);
-				printf ("[%s] Stopping...", self);
+			if (curr_time_msec - ts_msec > self->timeout) {
+				printf("[%s] Timeout! Could not connect to other peers.\n",self->name);
+				zyre_stop (self->local);
+				printf ("[%s] Stopping...", self->name);
 				zclock_sleep (100);
-				printf (" done\n");
-				zyre_destroy (&local);
-				//  @end
-				printf ("[%s] SHUTDOWN\n", self);
+				zyre_destroy (&self->local);
+				printf ("[%s] SHUTDOWN\n", self->name);
 				return 0;
 			}
 		} else {
-			printf ("[%s] could not get current time\n", self);
+			printf ("[%s] could not get current time\n", self->name);
 		}
     	zclock_sleep (1000);
     }
@@ -297,15 +367,19 @@ int main(int argc, char *argv[]) {
 
     printf("\n");
 	printf("#########################################\n");
-	printf("[%s] Sending Query for RSG Root Node\n",self);
+	printf("[%s] Sending Query for RSG Root Node\n",self->name);
 	printf("#########################################\n");
 	printf("\n");
-	json_t* query_params;
-	char *msg = send_query("GET_ROOT_NODE",query_params);
-	zyre_shouts(local, localgroup, "%s", msg);
-	printf("[%s] Sent msg: %s \n",self,msg);
-	// add it to query list
-
+	json_t* query_params = NULL;
+	char *msg = send_query(self,"GET_ROOT_NODE",query_params);
+	zyre_shouts(self->local, self->localgroup, "%s", msg);
+	printf("[%s] Sent msg: %s \n",self->name,msg);
+	printf("[%s] Queries in queue: %d \n",self->name,zlist_size (self->query_list));
+	//wait for query to be answered
+	//while (zlist_size (self->query_list) > 0){
+		void *which = zpoller_wait (self->poller, ZMQ_POLL_MSEC);
+		//if (which) {
+	//}
 
 
 
@@ -591,21 +665,16 @@ int main(int argc, char *argv[]) {
 //     		zclock_sleep (1000);
 //     	}
 
-    //free memory of all items from the query list
-    query_t *it;
-    while(zlist_size (query_list) > 0){
-    	it = (query_t*) zlist_pop(query_list);
-    	free(it->uid);
-    	free(it->msg);
-    }
 
-    zyre_stop (local);
-	printf ("[%s] Stopping...", self);
+
+    zyre_stop (self->local);
+	printf ("[%s] Stopping zyre.\n", self->name);
 	zclock_sleep (100);
-	printf (" done\n");
-    zyre_destroy (&local);
+    zyre_destroy (&self->local);
+    printf ("[%s] Destroying component.\n", self->name);
+    destroy_component(&self);
     //  @end
-    printf ("[%s] SHUTDOWN\n", self);
+    printf ("SHUTDOWN\n");
     return 0;
 }
 
