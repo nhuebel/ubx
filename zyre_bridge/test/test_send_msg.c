4441508627e05a9fefef6517c16996b8a0b91f81
Nico_Huebel <nico.huebel@kuleuven.be>
2016-08-11 20:03:49 +0200

    added function and test for update queries

--- zyre_bridge/test/test_send_msg.c
+++ zyre_bridge/test/test_send_msg.c
@@ -18,8 +18,9 @@ typedef struct _query_t {
 } query_t;
 
 typedef struct _component_t {
-	const char* name;
+	const char *name;
 	const char *localgroup;
+	char *RSG_parent;
 	zyre_t *local;
 	json_t *config;
 	zpoller_t *poller;
@@ -223,52 +224,11 @@ int decode_json(char* message, json_msg_t *result) {
     return 0;
 }
 
-//char* send_msg() {
-//	/**
-//	 * creates a world model update msg
-//	 *
-//	 * @return the string encoded JSON msg that can be sent directly via zyre. Must be freed by user! Returns NULL if wrong json types are passed in.
-//	 */
-//
-//    json_t *pl;
-//    pl = json_object();
-//    json_object_set(pl, "@worldmodeltype", json_string("RSGUpdate"));
-//    json_object_set(pl, "operation", json_string("CREATE"));
-//	json_object_set(pl, "parentId", json_string("e379121f-06c6-4e21-ae9d-ae78ec1986a1"));
-//    json_t *node;
-//    node = json_object();
-//    json_object_set(node, "@graphtype", json_string("Node"));
-//	zuuid_t *uuid = zuuid_new ();
-//	assert(uuid);
-//	//printf("UUID: %s\n",zuuid_str_canonical(uuid));
-//    json_object_set(node, "id", json_string(zuuid_str_canonical(uuid)));
-//	json_t *attributes;
-//    attributes = json_array();
-//	json_t *kv;
-//	kv = json_object();
-//	json_object_set(kv, "key", json_string("name"));
-//	json_object_set(kv, "value", json_string("myNode"));
-//	json_array_append_new(attributes, kv);
-//	kv = json_object();
-//	json_object_set(kv, "key", json_string("comment"));
-//	json_object_set(kv, "value", json_string("Some human readable comment on the node."));
-//	json_array_append_new(attributes, kv);
-//	json_object_set(node, "attributes", attributes);
-//    json_object_set(pl, "node", node);
-//    char* ret = json_dumps(pl, JSON_ENCODE_ANY);
-//
-//	json_decref(kv);
-//	json_decref(attributes);
-//	json_decref(node);
-//    json_decref(pl);
-//    return ret;
-//}
-
 char* send_query(component_t* self, char* query_type, json_t* query_params) {
 	/**
 	 * creates a query msg for the world model and adds it to the query list
 	 *
-	 * @param string query_type as string containing one of the availble query types ["GET_NODES", "GET_NODE_ATTRIBUTES", "GET_NODE_PARENTS", "GET_GROUP_CHILDREN", "GET_ROOT_NODE", "GET_REMOTE_ROOT_NODES", "GET_TRANSFORM", "GET_GEOMETRY", "GET_CONNECTION_SOURCE_IDS", "GET_CONNECTION_TARGET_IDS"]
+	 * @param string query_type as string containing one of the available query types ["GET_NODES", "GET_NODE_ATTRIBUTES", "GET_NODE_PARENTS", "GET_GROUP_CHILDREN", "GET_ROOT_NODE", "GET_REMOTE_ROOT_NODES", "GET_TRANSFORM", "GET_GEOMETRY", "GET_CONNECTION_SOURCE_IDS", "GET_CONNECTION_TARGET_IDS"]
 	 * @param json_t query_params json object containing all information required by the query; check the rsg-query-schema.json for details
 	 *
 	 * @return the string encoded JSON msg that can be sent directly via zyre. Must be freed by user! Returns NULL if wrong json types are passed in.
@@ -309,6 +269,61 @@ char* send_query(component_t* self, char* query_type, json_t* query_params) {
 
     char* ret = json_dumps(env, JSON_ENCODE_ANY);
 	
+	json_decref(env);
+    json_decref(pl);
+    return ret;
+}
+
+char* send_update(component_t* self, char* operation, json_t* update_params) {
+	/**
+	 * creates an update msg for the world model and adds it to the query list
+	 *
+	 * @param string operation as string containing one of the available operations ["CREATE","CREATE_REMOTE_ROOT_NODE","CREATE_PARENT","UPDATE_ATTRIBUTES","UPDATE_TRANSFORM","UPDATE_START","UPDATE_END","DELETE_NODE","DELETE_PARENT"]
+	 * @param json_t update_params json object containing all information required by the update; check the rsg-update-schema.json for details
+	 *
+	 * @return the string encoded JSON msg that can be sent directly via zyre. Must be freed by user! Returns NULL if wrong json types are passed in.
+	 */
+
+	// create the payload, i.e., the query
+    json_t *pl;
+    pl = json_object();
+    json_object_set(pl, "@worldmodeltype", json_string("RSGUpdate"));
+    json_object_set(pl, "operation", json_string(operation));
+    json_object_set(pl, "node", json_string(operation));
+	zuuid_t *uuid = zuuid_new ();
+	assert(uuid);
+    json_object_set(pl, "queryId", json_string(zuuid_str_canonical(uuid)));
+
+    if (!json_object_get(update_params,"node")) {
+    	printf("[%s:send_update] No node object on parameters",self->name);
+    	return NULL;
+    }
+	if (json_object_size(update_params)>0) {
+		const char *key;
+		json_t *value;
+		json_object_foreach(update_params, key, value) {
+			json_object_set(pl, key, value);
+		}
+	}
+	// pack it into the standard msg envelope
+	json_t *env;
+    env = json_object();
+	json_object_set(env, "metamodel", json_string("SHERPA"));
+	json_object_set(env, "model", json_string("RSGQuery"));
+	json_object_set(env, "type", json_string("RSGQuery"));
+	json_object_set(env, "payload", pl);
+
+	// add it to the query list
+	json_msg_t *msg = (json_msg_t *) zmalloc (sizeof (json_msg_t));
+	msg->metamodel = strdup("SHERPA");
+	msg->model = strdup("RSGQuery");
+	msg->type = strdup("RSGQuery");
+	msg->payload = strdup(json_dumps(pl, JSON_ENCODE_ANY));
+	query_t * q = query_new(zuuid_str_canonical(uuid), zyre_uuid(self->local), msg, NULL);
+	zlist_append(self->query_list, q);
+
+    char* ret = json_dumps(env, JSON_ENCODE_ANY);
+
 	json_decref(env);
     json_decref(pl);
     return ret;
@@ -373,6 +388,7 @@ void handle_shout(component_t *self, zmsg_t *msg) {
 				while (it != NULL) {
 					if (streq(it->uid,json_string_value(json_object_get(payload,"queryId")))) {
 						///TODO: how does update result message from sebastian look like? and what to do with it?
+						printf("[%s] received answer to query %s:\n %s\n ", self->name,it->uid,result->payload);
 						query_t *dummy = it;
 						it = zlist_next(self->query_list);
 						zlist_remove(self->query_list,dummy);
@@ -391,7 +407,7 @@ void handle_shout(component_t *self, zmsg_t *msg) {
 				query_t *it = zlist_first(self->query_list);
 				while (it != NULL) {
 					if (streq(it->uid,json_string_value(json_object_get(payload,"queryId")))) {
-						///TODO: what to do with the query result??
+						printf("[%s] received answer to query %s of type %s:\n Query:\n %s\n Result:\n %s ", self->name,it->uid,result->type,it->msg->payload, result->payload);
 						query_t *dummy = it;
 						it = zlist_next(self->query_list);
 						zlist_remove(self->query_list,dummy);
@@ -410,7 +426,7 @@ void handle_shout(component_t *self, zmsg_t *msg) {
 				query_t *it = zlist_first(self->query_list);
 				while (it != NULL) {
 					if (streq(it->uid,json_string_value(json_object_get(payload,"queryId")))) {
-						///TODO: what to do with the function block result
+						printf("[%s] received answer to query %s of type %s:\n Query:\n %s\n Result:\n %s ", self->name,it->uid,result->type,it->msg->payload, result->payload);
 						query_t *dummy = it;
 						it = zlist_next(self->query_list);
 						zlist_remove(self->query_list,dummy);
@@ -559,145 +575,80 @@ int main(int argc, char *argv[]) {
 	}
 
 
-
-
-//	void *which = zpoller_wait (poller, ZMQ_POLL_MSEC);
-//	if (which) {
-//		printf("[%s] local data received!\n", self);
-//		zmsg_t *msg = zmsg_recv (which);
-//		if (!msg) {
-//			printf("[%s] interrupted!\n", self);
-//			return -1;
-//		}
-//		//reset timeout
-//		if (clock_gettime(CLOCK_MONOTONIC,&ts)) {
-//			printf("[%s] Could not assign time stamp!\n",self);
-//		}
-//		char *event = zmsg_popstr (msg);
-//
-//		if (streq (event, "WHISPER")) {
-//			 assert (zmsg_size(msg) == 3);
-//			 char *peerid = zmsg_popstr (msg);
-//			 char *name = zmsg_popstr (msg);
-//			 char *message = zmsg_popstr (msg);
-//			 printf ("[%s] %s %s %s %s\n", self, event, peerid, name, message);
-//			 //printf("[%s] Received: %s from %s\n",self, event, name);
-//			json_msg_t *result = (json_msg_t *) zmalloc (sizeof (json_msg_t));
-//			if (decode_json(message, result) == 0) {
-//				// load the payload as json
-//				json_t *payload;
-//				json_error_t error;
-//				payload= json_loads(result->payload,0,&error);
-//				if(!payload) {
-//					printf("Error parsing JSON send_remote! line %d: %s\n", error.line, error.text);
-//				} else {
-//					const char *uid = json_string_value(json_object_get(payload,"UID"));
-//					//TODO:does this string need to be freed?
-//					if (!uid){
-//						printf("[%s] Received msg without UID!\n", self);
-//					} else {
-//						// search through stored list of queries and check if this query corresponds to one we have sent
-//						query_t *it = zlist_first(query_list);
-//						int found_UUID = 0;
-//						while (it != NULL) {
-//							if streq(it->uid, uid) {
-//								printf("[%s] Received reply to query %s.\n", self, uid);
-//								if (streq(result->type,"peer-list")){
-//									printf("Received peer list: %s\n",result->payload);
-//									//TODO: search list for a wasp
-//									alive = 0;
-//
-//								} else if (streq(result->type,"communication_report")){
-//									printf("Received communication_report: %s\n",result->payload);
-//									/////////////////////////////////////////////////
-//									//Do something with the report
-//									if (json_is_true(json_object_get(payload,"success"))){
-//										printf("Yeay! All recipients have received the msg.\n");
-//									} else {
-//										printf("Sending msg was not successful because of: %s\n",json_string_value(json_object_get(payload,"error")));
-//									}
-//									/////////////////////////////////////////////////
-//
-//									if (streq(json_string_value(json_object_get(payload,"error")),"Unknown recipients")){
-//										//This is really not how coordination in a program should be done -> TODO: clean up
-//
-//									}
-//
-//								}
-//								found_UUID = 1;
-//								zlist_remove(query_list,it);
-//								//TODO: make sure the data of that query is properly freed
-//							}
-//							it = zlist_next(query_list);
-//						}
-//						if (found_UUID == 0) {
-//							printf("[%s] Received a msg with an unknown UID!\n", self);
-//						}
-//					}
-//					json_decref(payload);
-//				}
-//
-//
-//
-//			} else {
-//				printf ("[%s] message could not be decoded\n", self);
-//			}
-//			free(result);
-//			zstr_free(&peerid);
-//			zstr_free(&name);
-//			zstr_free(&message);
-//		} else {
-//			printf ("[%s] received %s msg\n", self, event);
-//		}
-//		zstr_free (&event);
-//		zmsg_destroy (&msg);
-//	} else {
-//		if (!clock_gettime(CLOCK_MONOTONIC,&curr_time)) {
-//			// if timeout, stop component
-//			double curr_time_msec = curr_time.tv_sec*1.0e3 +curr_time.tv_nsec*1.0e-6;
-//			double ts_msec = ts.tv_sec*1.0e3 +ts.tv_nsec*1.0e-6;
-//			if (curr_time_msec - ts_msec > timeout) {
-//				printf("[%s] Timeout! No msg received for %i msec.\n",self,timeout);
-//				break;
-//			}
-//		} else {
-//			printf ("[%s] could not get current time\n", self);
-//		}
-//		printf ("[%s] waiting for a reply. Could execute other code now.\n", self);
-//		zclock_sleep (1000);
-//	}
-
-
-
 /**TODO:
  * * test why there is no query result with sebastian
  * * wait for return of query result to know root node
- * * send some updates -> write update function
- * * send some some queries -> send_query is written
- * * send some fnction_block_calls -> write function for that
+ * * send some updates
+ * * send some some queries
+ * * send some fnction_block_calls
  * * store all these queries
  * * wait till all queries are resolved
  * * make sure that some return errors
+ *
+ * ===============
+ * Missing:
+ * * set self->RSG_parent from initial root node query
+ * * function to create fnction_block_calls msg
+ * * rest of main running till all queries are served or timeout
  */
 
 
-//	int i;
-//    for( i = 0; i < no_of_updates; i++){
-//		printf("\n");
-//		printf("#########################################\n");
-//		printf("[%s] Sending Update %d\n",self,i);
-//		printf("#########################################\n");
-//		printf("\n");
-//		char *msg = send_msg();
-//		if (msg) {
-//			zyre_shouts(local, localgroup, "%s", msg);
-//			printf("[%s] Sent msg: %s \n",self,msg);
-//			zstr_free(&msg);
-//		} else {
-//			alive = false;
-//		}
-//		free(msg);
-//	}
+
+
+	int i;
+    for( i = 0; i < self->no_of_updates; i++){
+		printf("\n");
+		printf("#########################################\n");
+		printf("[%s] Sending Update %d\n",self->name,i);
+		printf("#########################################\n");
+		printf("\n");
+
+		// Example update msg
+		//		{
+		//		  "@worldmodeltype": "RSGUpdate", <- done by function
+		//		  "operation": "CREATE", <- done by function
+		//		  "node": {
+		//			  "@graphtype": "Node",
+		//			  "id": "8f3ba6f4-5c70-46ec-83af-0d5434953e5f",
+		//			  "attributes": [
+		//				{"key": "name", "value": "robot_1"},
+		//				{"key": "comment", "value": "none"},
+		//			  ],
+		//			},
+		//		  },
+		//		  "parentId": "193db306-fd8c-4eb8-a3ab-36910665773b",
+		//		}
+		json_t* update_params = json_object();
+		json_t* node = json_object();
+		json_t* attributes = json_array();
+		json_t* attribute1 = json_object();
+		json_object_set(attribute1,"key",json_string("name"));
+		json_object_set(attribute1,"value",json_string("robot_1"));
+		json_t* attribute2 = json_object();
+		json_object_set(attribute2,"key",json_string("comment"));
+		json_object_set(attribute2,"value",json_string("none"));
+		json_array_append(attributes, attribute1);
+		json_array_append(attributes, attribute2);
+		zuuid_t *node_id = zuuid_new ();
+		assert(node_id);
+		json_object_set(node,"@graphtype",json_string("Node"));
+		json_object_set(node,"id",json_string(zuuid_str_canonical(node_id)));
+		json_object_set(node,"attributes",attributes);
+		json_object_set(update_params,"node",node);
+		json_object_set(update_params,"parentId",json_string(self->RSG_parent));
+		char *msg = send_update(self, "CREATE",update_params);
+		if (msg) {
+			zyre_shouts(self->local, self->localgroup, "%s", msg);
+			printf("[%s] Sent msg: %s \n",self->name,msg);
+			zstr_free(&msg);
+		} else {
+			printf("[%s] Could not generate RSG update msg.\n",self->name);
+		}
+		free(msg);
+		json_decref(attributes);
+		json_decref(node);
+		json_decref(update_params);
+	}
 	
 //	for( i = 0; i < no_of_queries; i++){
 //		printf("\n");
